cdb3359756eb579ed86e69abfb0b9952417a5bbd
SananAlioglu <sananali0209@gmail.com>
2019-07-30 22:05:20 +0400

    Exercise1 completed

---
+++ Part-1/src/Exercise1.java
@@ -0,0 +1,8 @@
+//Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.print("Sanan Alioglu");
+    }
+
+}
---
+++ Part-1/src/Exercise2.java
@@ -0,0 +1,5 @@
+/**
+ * @author Sanan
+ */
+public class Exercise2 {
+}
