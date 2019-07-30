88c5eea7bed482fe6067adc50fec0e14986241ce
SananAlioglu <sananali0209@gmail.com>
2019-07-30 23:16:01 +0400

    Exercise11 completed

---
+++ Part-1/src/Exercise11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * @author Sanan
+ */
+ //Write a Java program to print the area and perimeter of a circle
+public class Exercise11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("input radius = ");
+        double r = sc.nextDouble();
+        System.out.println("Perimeter is:" + 2 * Math.PI * r);
+        System.out.println("Area is:" + Math.PI * Math.pow(r,2));
+    }
+}
